717bdd777b9df5e48dfaa7dfdb24f64930c46cb1
Ponderfly <treeson@foxmail.com>
2024-10-27 11:32:15 +0800

    增加扫描中使用Ctrl+C取消扫描并应用已扫描到IP

--- src/GoogleTranslateIpCheck/GoogleTranslateIpCheck/Program.cs
+++ src/GoogleTranslateIpCheck/GoogleTranslateIpCheck/Program.cs
@@ -9,6 +9,7 @@ using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 
 const string configFile = "config.json";
+const string version = "1.9"; 
 Console.WriteLine("如果支持IPv6推荐优先使用,使用参数 -6 启动");
 var config = new Config();
 if (File.Exists(configFile))
@@ -16,14 +17,37 @@ if (File.Exists(configFile))
     try
     {
         var json = File.ReadAllText(configFile);
-        config = JsonSerializer.Deserialize(json, ConfigJsonContext.Default.Config);
+        var options = new JsonSerializerOptions
+        {
+            ReadCommentHandling = JsonCommentHandling.Skip,
+            AllowTrailingCommas = true
+        };
+        var context = new ConfigJsonContext(options);
+        config = JsonSerializer.Deserialize(json, context.Config);
     }
     catch
     {
         Console.WriteLine("读取配置出错,将采用默认配置");
     }
 }
-
+var ctsTest = new CancellationTokenSource();
+var ctsScan = new CancellationTokenSource();
+bool isScan = false, isFirstCancel = true;
+Console.CancelKeyPress += (sender, e) => 
+{
+    e.Cancel = true;
+    if (isFirstCancel && isScan)
+    {
+        ctsScan.Cancel();
+        isFirstCancel = false;
+        Console.WriteLine("扫描操作已取消!");
+    }
+    else
+    {
+        ctsTest.Cancel();
+        Console.WriteLine("检测操作已取消!");
+    }
+};
 var autoSet = false;
 var isIPv6 = false;
 var ipFile = "ip.txt";
@@ -35,29 +59,43 @@ if (args.Length > 0)
         isIPv6 = true;
         ipFile = "IPv6.txt";
     }
-
+    if (args.Any(x => "-v".Equals(x, StringComparison.OrdinalIgnoreCase)))
+    {
+        Console.WriteLine($"版本号: {version}");
+        return;
+    }
     if (args.Any(x => "-y".Equals(x, StringComparison.OrdinalIgnoreCase)))
         autoSet = true;
     if (args.Any(x => "-s".Equals(x, StringComparison.OrdinalIgnoreCase)))
+    {
+        isScan = true;
         ips = await ScanIpAsync();
+    }
 }
 
 ips ??= await ReadIpAsync();
 if (ips is null || ips?.Count == 0)
-    ips = await ScanIpAsync();
+    if(!isScan)
+        ips = await ScanIpAsync();
 ConcurrentDictionary<string, long> times = new();
 Console.WriteLine();
 Console.WriteLine("开始检测IP响应时间");
 Console.WriteLine();
-await Parallel.ForEachAsync(ips!, new ParallelOptions()
+try
 {
-    MaxDegreeOfParallelism = config!.扫描并发数
-}, async (ip, _) =>
-{
+    await Parallel.ForEachAsync(ips!, new ParallelOptions()
+    {
+        MaxDegreeOfParallelism = config!.扫描并发数,
+        CancellationToken =  ctsTest.Token
+    }, async (ip, ct) =>
     {
         await TestIpAsync(ip);
-    }
-});
+    });
+}
+catch
+{
+    // ignored
+}
 
 if (times.IsEmpty)
 {
@@ -69,7 +107,7 @@ if (times.IsEmpty)
 Console.WriteLine();
 Console.WriteLine("检测IP完毕,按照响应时间排序结果");
 Console.WriteLine();
-var sortList = times.OrderByDescending(x => x.Value);
+var sortList = times.OrderByDescending(x => x.Value).ToList();
 foreach (var x in sortList)
     Console.WriteLine($"{x.Key}: 响应时间 {x.Value} ms");
 var bestIp = sortList.Last().Key;
@@ -117,8 +155,10 @@ async Task TestIpAsync(string ip)
     {
         try
         {
+            if(ctsTest.IsCancellationRequested)
+                return;
             sw.Start();
-            _ = await GetResultAsync(ip);
+            _ = await GetResultAsync(ip,ctsTest);
             sw.Stop();
             if (sw.ElapsedMilliseconds < time)
                 time = sw.ElapsedMilliseconds;
@@ -133,7 +173,7 @@ async Task TestIpAsync(string ip)
 
     if (flag)
     {
-        Console.WriteLine($"{ip}: 超时");
+        //Console.WriteLine($"{ip}: 超时");
         return;
     }
 
@@ -145,8 +185,6 @@ async Task<HashSet<string>?> ScanIpAsync()
 {
     Console.WriteLine("开始扫描可用IP,时间较长,请耐心等候");
     var listIp = new HashSet<string>();
-    CancellationTokenSource cts = new();
-    cts.Token.Register(() => { Console.WriteLine($"已经找到 {config!.IP扫描限制数量} 条IP,结束扫描"); });
     var IP段 = !isIPv6 ? config!.IP段 : config!.IPv6段;
     foreach (var ipRange in IP段)
     {
@@ -154,26 +192,26 @@ async Task<HashSet<string>?> ScanIpAsync()
         var _ips = new ConcurrentBag<string>();
         try
         {
-            var ipnetwork = IPNetwork2.Parse(ipRange);
+            var ipNetwork2 = IPNetwork2.Parse(ipRange);
             await
                 Parallel.ForEachAsync(
-                    ipnetwork.ListIPAddress(FilterEnum.Usable),
+                    ipNetwork2.ListIPAddress(FilterEnum.Usable),
                     new ParallelOptions()
                     {
                         MaxDegreeOfParallelism = config!.扫描并发数,
-                        CancellationToken = cts.Token
+                        CancellationToken = ctsScan.Token
                     },
                     async (ip, ct) =>
                     {
                         try
                         {
-                            var result = await GetResultAsync(ip.ToString());
+                            var result = await GetResultAsync(ip.ToString(),ctsScan);
                             if (!result)
                                 return;
                             Console.WriteLine($"{ip}");
                             _ips.Add(ip.ToString());
                             if (listIp.Count + _ips.Count >= config!.IP扫描限制数量)
-                                cts.Cancel();
+                                ctsScan.Cancel();
                         }
                         catch
                         {
@@ -182,7 +220,7 @@ async Task<HashSet<string>?> ScanIpAsync()
         }
         catch
         {
-            continue;
+            // ignored
         }
         finally
         {
@@ -197,14 +235,14 @@ async Task<HashSet<string>?> ScanIpAsync()
     return listIp;
 }
 
-async Task<bool> GetResultAsync(string ip)
+async Task<bool> GetResultAsync(string ip,CancellationTokenSource cts)
 {
     var str = !isIPv6 ? $"{ip}" : $"[{ip}]";
     var url = $@"https://{str}/translate_a/single?client=gtx&sl=zh-CN&tl=en&dt=t&q=你好";
     return (await url
         .WithHeader("host", config!.Hosts[0])
         .WithTimeout(config!.扫描超时)
-        .GetStringAsync()).Contains("Hello");
+        .GetStringAsync(cancellationToken : cts.Token)).Contains("Hello");
 }
 
 async Task<HashSet<string>?> ReadIpAsync()
@@ -304,7 +342,8 @@ async Task SetHostFileAsync()
 
 async Task SaveIpFileAsync()
 {
-    await File.WriteAllLinesAsync(ipFile, sortList.Reverse().Select(x => x.Key), Encoding.UTF8);
+    sortList.Reverse();
+    await File.WriteAllLinesAsync(ipFile, sortList.Select(x => x.Key), Encoding.UTF8);
 }
 
 async Task FlushDns()
@@ -334,17 +373,15 @@ async Task FlushDns()
 
     try
     {
-        using var process = new Process()
+        using var process = new Process();
+        process.StartInfo = new ProcessStartInfo()
         {
-            StartInfo = new ProcessStartInfo()
-            {
-                FileName = fileName,
-                Arguments = arguments,
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true
-            }
+            FileName = fileName,
+            Arguments = arguments,
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
         };
         process.Start();
         sb.Append(await process.StandardOutput.ReadToEndAsync());
@@ -361,10 +398,7 @@ async Task FlushDns()
 
 bool CheckIp(string ip)
 {
-    if (!isIPv6)
-        return RegexStuff.IpRegex().IsMatch(ip.Trim());
-    else
-        return RegexStuff.IPv6Regex().IsMatch(ip.Trim());
+    return !isIPv6 ? RegexStuff.IpRegex().IsMatch(ip.Trim()) : RegexStuff.IPv6Regex().IsMatch(ip.Trim());
 }
 
 public partial class RegexStuff
@@ -388,9 +422,10 @@ public class Config
             translate.googleapis.com
             translate.google.com
             translate-pa.googleapis.com
+            "jnn-pa.googleapis.com"
             """.Split(Environment.NewLine);
     public string[] IP段 { get; set; } =
-        """"
+        """
             142.250.0.0/15
             172.217.0.0/16
             172.253.0.0/16
@@ -398,9 +433,9 @@ public class Config
             72.14.192.0/18
             74.125.0.0/16
             216.58.192.0/19
-            """".Split(Environment.NewLine);
+            """.Split(Environment.NewLine);
     public string[] IPv6段 { get; set; } =
-        """"
+        """
             2404:6800:4008:c15::0/112
             2a00:1450:4001:802::0/112
             2a00:1450:4001:803::0/112
@@ -408,7 +443,7 @@ public class Config
             2a00:1450:4001:811::0/112
             2a00:1450:4001:827::0/112
             2a00:1450:4001:828::0/112
-            """".Split(Environment.NewLine);
+            """.Split(Environment.NewLine);
 }
 
 [JsonSerializable(typeof(Config))]
